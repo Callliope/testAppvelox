edcc33daa793caca0c236ca80eccd4181f7507ac
Maria Mihailovskaya <mihailovskayam33@gmail.com>
2022-06-25 23:43:52 +0300

    navbar is done

--- test-appvelox/src/components/navbar/Navbar.jsx
+++ test-appvelox/src/components/navbar/Navbar.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import "./navbar.css";
+import searchpic from "../../assets/search.png";
+import bellpic from "../../assets/bell.png";
+import eyepic from "../../assets/eye 1.png";
+import profilepic from "../../assets/profilepic.png";
+import openpic from '../../assets/open.png';
 
 
 
@@ -6,21 +12,29 @@ import React from 'react';
 export default function navbar() {
   return (
     <div > 
-    <nav class="navbar navbar-expand-lg navbarcolor ">
-  <div class="container-fluid">
-    <a class="navbar-brand textcolor" href="#">Мой профиль</a>
+    <nav class="navbar navbar-expand-lg navbarstyle ">
+  <div class="container-fluid" >
+    <a class="navbar-brand textcolor topText" href="#">Мой профиль</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Переключатель навигации">
       <span class="navbar-toggler-icon"></span>
     </button>
         <form class="d-flex">
         <a class="navbar-brand" href="#">
-      <img src='../..assets/search.png' width="30" height="24" class="d-inline-block align-text-top"></img>
+      <img src={searchpic} alt="search icon"  class="d-inline-block  iconStyle"></img>
     </a>
     <a class="navbar-brand" href="#">
-      <img src="test-appvelox\assets\bell.png"  width="30" height="24" class="d-inline-block align-text-top"></img>
+      <img src={bellpic} alt="bell icon"  class="d-inline-block  iconStyle"></img>
     </a>
     <a class="navbar-brand" href="#">
-      <img src="test-appvelox\assets\eye 1.png"  width="30" height="24" class="d-inline-block align-text-top"></img>
+      <img src={eyepic} alt='eye icon'  class="d-inline-block  iconStyle"></img>
+    </a>
+    <a class="navbar-brand" href="#">
+      <img src={profilepic} alt='Profile picture'  class="d-inline-block  profileStyle"></img>
+     
+    </a>
+    
+    <a class="navbar-brand" href="#">
+      <img src={openpic} alt='open icon'  class="d-inline-block  iconStyle"></img>
     </a>
 
     </form>
--- test-appvelox/src/components/sidebar/Sidebar.jsx
+++ test-appvelox/src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
-import "./sidebar.css"
+import React from 'react';
+import "./sidebar.css";
+import logopic from '../../assets/logo.png';
+import heartpic from '../../assets/heart (2) 1.png';
+import medicalpic from '../../assets/Vector.png'
 
 export default function sidebar() {
   return (
-    <div>sidebar</div>
+    <div>
+      <div class="sidenav">
+  <div class='sidebarBlock'><a href="#">Логотип</a></div>
+
+  <div class='sidebarBlock'>
+  <a  href="#">
+      <img src={heartpic} alt="search icon"  class=" iconStyle"></img>Профиль</a>
+  </div>
+
+  <div class='sidebarBlock'><a  href="#">
+      <img src={medicalpic} alt="search icon"  class=" iconStyle"></img>Врачи и клиники</a></div>
+
+  <div class='sidebarBlock'><a  href="#">
+      <img src={heartpic} alt="search icon"  class=" iconStyle"></img>Профиль</a></div>
+
+  <div class='sidebarBlock'><a  href="#">
+      <img src={heartpic} alt="search icon"  class=" iconStyle"></img>Тестирование</a></div>
+
+  <div class='sidebarBlock'><a  href="#">
+      <img src={heartpic} alt="search icon"  class=" iconStyle"></img>Полезно знать</a></div>
+
+    <div><button>Подать заявку</button></div>
+
+  <div><a href="#">Помощь</a></div>
+
+  <img src={logopic} alt="appvelox logo"></img>
+</div>
+    </div>
   )
 }
